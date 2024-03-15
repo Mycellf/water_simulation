34156f6ceab732d8e4a7499e9d552e49335f7228
Mycellf <jcouch210@gmail.com>
2024-03-14 20:52:15 -0800

    Added constructor for simulation_area

--- src/simulation_area.rs
+++ src/simulation_area.rs
@@ -1,9 +1,26 @@
 use macroquad::prelude::*;
+use nalgebra::Vector2;
 
 pub struct SimulationArea {
     pub simulation: Vec<Particle>,
     pub image: Image,
     pub texture: Texture2D,
+}
+
+impl SimulationArea {
+    pub fn new(size: Vector2<usize>) -> Self {
+        let element_count = size.x * size.y;
+        let simulation = (0..element_count).map(|_| Particle).collect();
+
+        let image = Image::gen_image_color(size.x as u16, size.y as u16, WHITE);
+        let texture = Texture2D::from_image(&image);
+
+        Self {
+            simulation,
+            image,
+            texture,
+        }
+    }
 }
 
 pub struct Particle;
