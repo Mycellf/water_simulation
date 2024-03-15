68162b50c276ccfd93e5980172c0d4251e926561
Mycellf <jcouch210@gmail.com>
2024-03-14 21:01:11 -0800

    Added the Particle struct's data

--- src/simulation_area.rs
+++ src/simulation_area.rs
@@ -10,7 +10,9 @@ pub struct SimulationArea {
 impl SimulationArea {
     pub fn new(size: Vector2<usize>) -> Self {
         let element_count = size.x * size.y;
-        let simulation = (0..element_count).map(|_| Particle).collect();
+        let simulation = (0..element_count)
+            .map(|_| Particle::new(ParticleType::Air))
+            .collect();
 
         let image = Image::gen_image_color(size.x as u16, size.y as u16, WHITE);
         let texture = Texture2D::from_image(&image);
@@ -23,4 +25,20 @@ impl SimulationArea {
     }
 }
 
-pub struct Particle;
+pub struct Particle {
+    pub particle_type: ParticleType,
+    pub pressure: u16,
+}
+
+impl Particle {
+    pub fn new(particle_type: ParticleType) -> Self {
+        Self {
+            particle_type,
+            pressure: 0,
+        }
+    }
+}
+
+pub enum ParticleType {
+    Air,
+}
